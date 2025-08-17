05224c36b042437a8762bcce43663467772f5647
emmy <52672675+emmaccode@users.noreply.github.com>
2025-08-17 11:36:40 -0600

    (finally) completed line autoindentation
    *jeeze* talk about a pain in the rear. I had to fix both the set and get cursor system for this, the biggest struggle was how HTML won't insert the <brs> right if it is the first br for the cell (if that makes sense). The silly solution to that problem is adding a break to the end of the source in those cases.
    ```julia
        if ~(contains(cell.source, "\n") || contains(cell.source, "<br>"))
            interior = component[:children]["cellinterior$(cell.id)"]
            inp = interior[:children]["cellinput$(cell.id)"]
            maincell::Component{:div} = inp[:children]["cell$(cell.id)"]
            maincell[:text] = maincell[:text] * "<br>"
        end
    ```
    
    Ugh. So, when it gets a <br> and isn't ready for one -- of all the things to do, it blurs. This would make the **first** enter NEVER WORK and unfocus the cell, very annoying, but after that the autocomplete would be normal. This was a nightmare, honestly! Hehe. I am happy with its state here, even if it isn't exactly perfect...  The last remaining feature to create will be the actual autocomplete insert functionality, which I think will actually be a lot easier. No idea as to whether or not this is unicode safe. The first iteration may not be.

--- src/OliveAutoComplete.jl
+++ src/OliveAutoComplete.jl
@@ -10,125 +10,80 @@ indent_after = ("begin", "function", "struct", "for", "if", "else", "elseif", "d
 function on_code_build(c::Connection, cm::ComponentModifier, oe::OliveExtension{:indent}, 
     cell::Cell{:code}, proj::Project{<:Any}, component::Component{:div}, km::ToolipsSession.KeyMap)
     suggest_box = div("autobox")
-    style!(suggest_box, "background-color" => "white", "border" => "2px solid #1e1e1e", 
-        "border-radius" => 3px)
+    style!(component[:children][1], "margin" => 0px, "padding" => 0percent)
+    style!(suggest_box, "background-color" => "white", "border" => "2px solid #3D3D3D", 
+        "border-radius" => 3px, "margin-left" => 28pt, "width" => 85percent, "border-top" => 0px, "border-top-left-radius" => 0px, "border-top-right-radius" => 0px, 
+        "min-height" => 16pt)
     insert!(component[:children], 2, suggest_box)
     ToolipsSession.bind(km, " ", :shift, prevent_default = true) do cm::ComponentModifier
-        alert!(cm, "worked")
+        alert!(cm, "will fill currently selected complete")
     end
-    ToolipsSession.bind(c, cm, component, "Enter", on = :up, prevent_default = false) do cm::ComponentModifier
+    if ~(contains(cell.source, "\n") || contains(cell.source, "<br>"))
+        interior = component[:children]["cellinterior$(cell.id)"]
+        inp = interior[:children]["cellinput$(cell.id)"]
+        maincell::Component{:div} = inp[:children]["cell$(cell.id)"]
+        maincell[:text] = maincell[:text] * "<br>"
+    end
+    ToolipsSession.bind(km, "Enter", prevent_default = false) do cm::ComponentModifier
         callback_comp::Component = cm["cell$(cell.id)"]
-        
         curr::String = callback_comp["text"]
-        #==
-        if curr[end] == '\n'
-            if curr == "\n"
-                return
-            end
-            curr = curr[1:end - 1]
-        end
-        ==#
-        last_n::Int64 = parse(Int64, callback_comp["caret"])
-        n::Int64 = length(curr)
-        @info "(debug messages)"
-        if last_n <= n
-            @warn replace(curr[1:last_n], " " => "-", "\n" => "|||") * "|CURSOR|" * replace(curr[last_n + 1:end], " " => "-", "\n" => "|||")
-        else
-            @warn "last_n > n :o"
-            throw("last_n: $(last_n), `n`: $n")
-        end
-        @warn replace(curr[last_n:last_n], "\n" => "YES")
-        @warn replace(curr[last_n:last_n], "\n" => "YES", " " => "SPACE")
-        previous_line_i = findprev("\n", curr, last_n)
-        if isnothing(previous_line_i)
-            @warn "no previous line"
-            previous_line_i = 1
-        else
-            previous_line_i = minimum(previous_line_i)
-        end
-        @warn "previous line: \n" * replace(curr[begin:previous_line_i], "\n" => "|||")
-
-        line_slice = curr[previous_line_i:last_n]
-        contains_indent::Bool = ~isnothing(findfirst(x -> contains(line_slice, x), indent_after))
-        contains_end = ~(isnothing(findfirst("end", line_slice)))
-        indentation_level = findfirst(c -> c != ' ', replace(line_slice, "\n" => ""))
-        if isnothing(indentation_level)
-            indentation_level = length(replace(line_slice, "\n" => ""))
-        else
-            indentation_level = minimum(indentation_level) - 1
-        end
-        @warn "current line: \n" * replace(line_slice, " " => "-", "\n" => "|||")
-        @info indentation_level
-        indent_level = ~(isnothing(indentation_level)) && indentation_level > 3 && replace(line_slice, "\n" => "")[1] == ' '
-        @info "continue indent?: " * string(indent_level)
-        if ~(indent_level)
-            msg = ""
-            if isnothing(indentation_level)
-                msg = "no indentation level $indentation_level"
-            elseif ~(indentation_level[end] > 3)
-                msg = "indentation level not > 3 $indentation_level"
-            else
-                msg = "failed level check: " * string(indentation_level) 
-            end
-            @warn "reason for no continued indent: " * msg
-        end
-        if contains_end && contains_indent
+        last_n::Int = parse(Int, callback_comp["caret"])
+        n::Int = length(curr)
+        if n == 0
+            return
+        elseif n < 2
             return
         end
-        if contains_end && indent_level
-            indentation_level -= 4
-        end
-        # TODO get last line, check for indent key-words, pre-indentation, and `end`.
-        #<br> is replaced, so `\n` is shorter by 2. Plus, JS index starts at 0
-        if contains_indent
-            addin = ""
-            position = last_n + 4
-            if indent_level
-                addin = join("&nbsp;" for x in 1:indentation_level)
-                position += indentation_level
+        # safety
+        if last_n > n + 1
+            last_n -= 1
+            if last_n > n + 1
+                @warn "caret beyond length"
             end
-            res = curr[1:last_n] * "\n    " * addin * curr[last_n + 1:length(curr)]
-            cell.source = res
-            set_text!(cm, "cell$(cell.id)", replace(cell.source, " " => "&nbsp;"))
-            @info "caret: " string(last_n) " len: " length(curr)
-            @info string(last_n + 5 + indentation_level)
-            cm["cell$(cell.id)"] = "caret" => position + 1
-            focus!(cm, "cell$(cell.id)")
-            @info "set cursor pos to $position"
-            Components.set_textdiv_cursor!(cm, "cell$(cell.id)", position)
-            
-        elseif contains_end && indent_level
-            @warn "suspected option"
-            @warn curr[previous_line_i:end]
-            indentation_level -= 4
-            offset = 0
-            if indentation_level > 3
-                cell.source = curr[1:last_n] * join(" " for x in 1:indentation_level) * "\n" * curr[last_n + 1:end]
-                offset = 5
-            else
-                cell.source = curr[1:previous_line_i] * curr[previous_line_i + 5:end] * "\n"
-                @warn cell.source
-                offset = 0
-            end
-            set_text!(cm, "cell$(cell.id)", replace(cell.source, " " => "&nbsp;"))
-            offlen = length(findall("\n", curr))
-            position = last_n + offset - 4 - offlen
-            cm["cell$(cell.id)"] = "caret" => position + offlen
-            @warn position
-            Components.set_textdiv_cursor!(cm, "cell$(cell.id)", position)
-        elseif indent_level
-            @info "INDENTED"
-            @info replace(join(" " for x in 1:indentation_level), " " => "-")
-            res = curr[1:last_n] * "\n" * join(" " for x in 1:indentation_level) * curr[last_n + 1:end]
-            cell.source = res
-            @info replace(cell.source, " " => "&nbsp;")
-            set_text!(cm, "cell$(cell.id)", replace(cell.source, " " => "&nbsp;"))
-            cm["cell$(cell.id)"] = "caret" => string(last_n + indentation_level)
-            focus!(cm, "cell$(cell.id)")
-            Components.set_textdiv_cursor!(cm, "cell$(cell.id)", last_n + indentation_level - length(findall("\n", curr)))
+            return
         end
+        before = curr[1:clamp(last_n, 1, n)]
+        after  = last_n <= n ? curr[last_n+1:end] : ""
+        lines = split(curr, '\n')
+        prev_line = isempty(lines) || length(lines) < 2 ? "" : lines[end - 1]
+        indent_spaces = length(match(r"^ *", prev_line).match)
+        current_line_idx = count(==('\n'), curr[1:clamp(last_n, 1, n)]) + 1
+        current_line = lines[current_line_idx]
+
+        contains_indent = any(contains(prev_line, x) for x in indent_after)
+        indent_spaces = length(match(r"^ *", prev_line).match)
+        cur_indent_spaces = length(match(r"^ *", current_line).match)
+        current_line_has_end = occursin(r"\bend\b", current_line)
+
+        new_indent::Int64 = indent_spaces
+        if contains_indent && ~(current_line_has_end)
+            new_indent += 4
+        end
+        # rule 2: if current line contains "end" and is at same indent as prev
+        newpos = nothing
+        if occursin(r"\bend\b", current_line)      
+            prev_indent_spaces = length(match(r"^ *", prev_line).match)
+            if cur_indent_spaces == indent_spaces && cur_indent_spaces > 3
+                @warn current_line
+                @warn prev_line
+                # Dedent "end" by stripping up to 4 spaces
+                stripped_line = replace(current_line, r"^ {0,4}" => "")
+                lines[current_line_idx] = stripped_line
+                new_indent -= 4
+            end
+        end
+        newline_indent = "<br>" * " "^new_indent
+        res = before * newline_indent * after
+        res_safe = replace(res, " " => "&nbsp;", "\n" => "<br>")
+        newpos = last_n + 1 + new_indent
+        # update component
+        @info res_safe
+        set_text!(cm, "cell$(cell.id)", res_safe)
+        cm["cell$(cell.id)"] = "caret" => string(newpos)
+        Components.set_textdiv_cursor!(cm, "cell$(cell.id)", newpos + 1)
     end
 end
 
+    
 
 end # module OliveAutoComplete
